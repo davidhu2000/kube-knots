2cdacf0d304048c033eb16447fb5467828ebdaf1
David Hu <davidhu314@gmail.com>
2023-01-08 22:37:38 -0800

    feat: add functionality to get pods from the UI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,9 +9,33 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use k8s_openapi::api::core::v1::Pod;
+
+use kube::{
+    api::{Api, ListParams, ObjectList},
+    Client,
+};
+
+#[tauri::command]
+async fn get_pods() -> ObjectList<Pod> {
+    let client = Client::try_default().await.unwrap();
+
+    let pods: Api<Pod> = Api::default_namespaced(client);
+
+    let lp = ListParams::default();
+    let pod = pods.list(&lp).await.unwrap();
+
+    // log pod to terminal
+    // println!("{:#?}", pod);
+
+    pod
+
+    // Into::<ObjectList<Pod>>::into(pod);
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_pods])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
