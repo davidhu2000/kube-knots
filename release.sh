0355b6e9f38ea0152e7192c87971edd5e24020a3
David Hu <davidhu314@gmail.com>
2023-01-09 22:10:32 -0800

    chore: fix release script file version interpolation

--- release.sh
+++ release.sh
@@ -6,4 +6,4 @@ yarn tauri build
 version=$(grep version package.json | awk -F \" '{print $4}')
 
 echo "releasing version: $version"
-gh release create $version "src-tauri/target/release/bundle/dmg/Kube Knots_$version_x64.dmg" --generate-notes
+gh release create $version "src-tauri/target/release/bundle/dmg/Kube Knots_${version}_x64.dmg" --generate-notes
