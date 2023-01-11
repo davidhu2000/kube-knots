956d9b12e0c95fd2a4ac501c41066f9e4d57d52a
David Hu <davidhu314@gmail.com>
2023-01-11 07:41:17 -0800

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,13 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "tailwindcss"],
   root: true,
-  ignorePatterns: ["src-tauri/target", "dist"],
+  ignorePatterns: ["src-tauri/target", "dist", "*.js"],
   rules: {
     "@typescript-eslint/consistent-type-imports": "error",
   },
