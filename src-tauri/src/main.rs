979617d7ca3b98bdcbc452d4551e3c30ec582f48
David Hu <davidhu314@gmail.com>
2023-01-10 21:24:43 -0800

    feat: add query to get list of namespace, filter pods by namespace

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,14 @@
     windows_subsystem = "windows"
 )]
 
+mod namespaces;
 mod pods;
 
-use crate::pods::get_pods;
+use crate::{namespaces::get_namespaces, pods::get_pods};
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_pods])
+        .invoke_handler(tauri::generate_handler![get_pods, get_namespaces])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
