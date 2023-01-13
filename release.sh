92821af8e994707f4ac5c5f26390b46b9f7015d4
David Hu <davidhu314@gmail.com>
2023-01-12 21:26:45 -0800

    feat: update release script to build for m1 macs

--- release.sh
+++ release.sh
@@ -2,8 +2,12 @@
 # this script should be replaces by github actions
 
 npm version patch
-yarn tauri build
+yarn tauri build --target aarch64-apple-darwin
+yarn tauri build --target x86_64-apple-darwin
 version=$(grep version package.json | awk -F \" '{print $4}')
 
 echo "releasing version: $version"
-gh release create $version "src-tauri/target/release/bundle/dmg/Kube Knots_${version}_x64.dmg" --generate-notes
+gh release create $version \
+ "src-tauri/target/x86_64-apple-darwin/release/bundle/dmg/kube-knots_${version}_x64.dmg"  \ 
+ "src-tauri/target/aarch64-apple-darwin/release/bundle/dmg/kube-knots_${version}_aarch64.dmgg"  \ 
+ --generate-notes
