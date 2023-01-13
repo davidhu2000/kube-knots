e0ad1cc265576f31448fe919018bb434e1241a72
David Hu <davidhu314@gmail.com>
2023-01-12 21:39:53 -0800

    chore: fix release script to upload both m1/intel dmg files

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ yarn tauri build --target x86_64-apple-darwin
 version=$(grep version package.json | awk -F \" '{print $4}')
 
 echo "releasing version: $version"
-gh release create $version \
- "src-tauri/target/x86_64-apple-darwin/release/bundle/dmg/kube-knots_${version}_x64.dmg"  \ 
- "src-tauri/target/aarch64-apple-darwin/release/bundle/dmg/kube-knots_${version}_aarch64.dmgg"  \ 
- --generate-notes
+
+macos_intel_path="src-tauri/target/x86_64-apple-darwin/release/bundle/dmg"
+macos_m1_path="src-tauri/target/aarch64-apple-darwin/release/bundle/dmg"
+gh release create $version "$macos_intel_path/kube-knots_${version}_x64.dmg" "$macos_m1_path/kube-knots_${version}_aarch64.dmg" --generate-notes
