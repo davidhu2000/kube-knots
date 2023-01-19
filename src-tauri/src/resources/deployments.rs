3d6159d2ff077be3aabbdeaca831df7835ecab70
David Hu <davidhu314@gmail.com>
2023-01-18 20:55:37 -0800

    scale-deployment

--- src-tauri/src/resources/deployments.rs
+++ src-tauri/src/resources/deployments.rs
@@ -1,5 +1,9 @@
 use k8s_openapi::api::apps::v1::Deployment;
-use kube::{api::ListParams, core::ObjectList, Api};
+use kube::{
+    api::{ListParams, Patch, PatchParams},
+    core::ObjectList,
+    Api,
+};
 
 use super::internal::get_api;
 
@@ -23,5 +27,24 @@ pub async fn restart_deployment(namespace: Option<String>, name: String) -> bool
             return false;
         }
     };
+    return result;
+}
+
+#[tauri::command]
+pub async fn scale_deployment(namespace: Option<String>, name: String, replicas: u8) -> bool {
+    let api: Api<Deployment> = get_api(namespace).await;
+    let spec = serde_json::json!({ "spec": { "replicas": replicas }});
+    let pp = PatchParams::default();
+    let patch = Patch::Merge(&spec);
+    let resource = api.patch_scale(&name, &pp, &patch).await;
+
+    let result = match resource {
+        Ok(_resource) => true,
+        Err(err) => {
+            println!("Error scaling deployment: {}", err);
+            return false;
+        }
+    };
+
     return result;
 }
---
+++ src/deployments/scale-deployment-modal.tsx
@@ -0,0 +1,66 @@
+import { Dialog } from "@headlessui/react";
+import { type V1Deployment } from "@kubernetes/client-node";
+import { useMutation } from "@tanstack/react-query";
+import { invoke } from "@tauri-apps/api";
+import { useState } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  handleClose: () => void;
+  deployment: V1Deployment;
+}
+export function ScaleDeploymentModal({ isOpen, handleClose, deployment }: ModalProps): JSX.Element {
+  const [replicas, setReplicas] = useState<number>(deployment.spec?.replicas || 0);
+
+  const scaleMutation = useMutation({
+    mutationFn: (replicas: number) => {
+      return invoke("scale_deployment", {
+        namespace: deployment.metadata?.namespace,
+        name: deployment.metadata?.name,
+        replicas,
+      });
+    },
+    onSuccess: (_data, variables) => {
+      handleClose();
+      alert(`Scaled deployment ${deployment.metadata?.name} to ${variables} replicas`);
+    },
+  });
+
+  return (
+    <Dialog as="div" className="relative z-10" onClose={handleClose} open={isOpen}>
+      <div className="fixed inset-0 bg-gray-500/75 transition-opacity" />
+
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <Dialog.Panel className="relative rounded-lg bg-gray-100 p-4 text-left shadow-xl">
+            <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+              Scale {deployment.metadata?.name}
+            </Dialog.Title>
+
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">
+                Current replica count: {deployment.spec?.replicas}
+              </p>
+            </div>
+
+            <input
+              onChange={(e) => setReplicas(parseInt(e.target.value ?? ""))}
+              type="number"
+              value={replicas}
+              className="mt-4 block w-full rounded-md border-gray-300 focus:ring-indigo-500"
+              step={1}
+              min={0}
+            />
+
+            <button
+              className="mt-4 w-full rounded-md bg-gray-200 px-4 py-2 text-base text-gray-800 shadow-sm hover:bg-gray-300"
+              onClick={() => scaleMutation.mutate(replicas)}
+            >
+              Scale
+            </button>
+          </Dialog.Panel>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
