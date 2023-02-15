a2066a86734a7588496a3eabce3339d4fb586a27
David Hu <davidhu314@gmail.com>
2023-02-14 22:01:43 -0800

    fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,26 +8,21 @@ mod internal;
 pub mod networking;
 pub mod workloads;
 
-use crate::{
-    networking::{ingresses, services},
-    workloads::{cron_jobs, deployments, jobs, namespaces, pods, replica_sets, stateful_sets},
-};
-
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            cron_jobs::get_cron_jobs,
-            deployments::get_deployments,
-            deployments::restart_deployment,
-            deployments::scale_deployment,
-            ingresses::get_ingresses,
-            jobs::get_jobs,
-            pods::get_pod_logs,
-            pods::get_pods,
-            namespaces::get_namespaces,
-            replica_sets::get_replica_sets,
-            services::get_services,
-            stateful_sets::get_stateful_sets
+            networking::ingresses::get_ingresses,
+            networking::services::get_services,
+            workloads::cron_jobs::get_cron_jobs,
+            workloads::deployments::get_deployments,
+            workloads::deployments::restart_deployment,
+            workloads::deployments::scale_deployment,
+            workloads::jobs::get_jobs,
+            workloads::pods::get_pod_logs,
+            workloads::pods::get_pods,
+            workloads::namespaces::get_namespaces,
+            workloads::replica_sets::get_replica_sets,
+            workloads::stateful_sets::get_stateful_sets,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
