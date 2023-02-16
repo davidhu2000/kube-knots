d99a3c09e4181ac8733bb2540aea92006d785d8e
David Hu <davidhu314@gmail.com>
2023-02-15 21:47:19 -0800

    updatesetting

--- src/settings/settings.tsx
+++ src/settings/settings.tsx
@@ -1,5 +1,4 @@
 import { RadioGroup } from "@headlessui/react";
-import { CheckCircleIcon } from "@heroicons/react/20/solid";
 
 import { BaseModal, ModalButton } from "../components/modal";
 import { useTheme } from "../providers/theme-provider";
@@ -11,54 +10,39 @@ export function Settings({ isOpen, handleClose }: { isOpen: boolean; handleClose
 
   return (
     <BaseModal isOpen={isOpen} handleClose={handleClose} title="Settings">
-      <div className="w-full px-4 py-16">
-        <div className="mx-auto w-full max-w-md">
-          <RadioGroup value={theme} onChange={changeTheme}>
-            <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
-            <div className="space-y-2">
-              {themes.map((theme) => (
-                <RadioGroup.Option
-                  key={theme}
-                  value={theme}
-                  className={({ active, checked }) =>
-                    `${
-                      active
-                        ? "ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300"
-                        : ""
-                    }
-                  ${checked ? "bg-sky-900 bg-opacity-75 text-white" : "bg-white"}
-                    relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
-                  }
-                >
-                  {({ active, checked }) => (
-                    <>
-                      <div className="flex w-full items-center justify-between">
-                        <div className="flex items-center">
-                          <div className="text-sm">
-                            <RadioGroup.Label
-                              as="p"
-                              className={`font-medium  ${checked ? "text-white" : "text-gray-900"}`}
-                            >
-                              {theme}
-                            </RadioGroup.Label>
-                          </div>
-                        </div>
-                        {checked && (
-                          <div className="shrink-0 text-white">
-                            <CheckCircleIcon className="h-6 w-6" />
-                          </div>
-                        )}
-                      </div>
-                    </>
-                  )}
-                </RadioGroup.Option>
-              ))}
-            </div>
-          </RadioGroup>
+      <RadioGroup value={theme} onChange={changeTheme} className="p-4">
+        <RadioGroup.Label className="text-gray-900 dark:text-gray-100">Theme</RadioGroup.Label>
+        <div className="grid grid-cols-3 gap-2">
+          {themes.map((theme) => (
+            <RadioGroup.Option
+              key={theme}
+              value={theme}
+              className={({ checked }) =>
+                `${
+                  checked ? "bg-gray-800 text-white" : "bg-white"
+                } flex cursor-pointer rounded-lg p-4 shadow-md w-32 h-12`
+              }
+            >
+              {({ checked }) => (
+                <div className="flex w-full items-center justify-between">
+                  <div className="flex items-center">
+                    <div className="text-sm">
+                      <RadioGroup.Label
+                        as="p"
+                        className={`font-medium  ${checked ? "text-white" : "text-gray-900"}`}
+                      >
+                        {theme}
+                      </RadioGroup.Label>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </RadioGroup.Option>
+          ))}
         </div>
-      </div>
+      </RadioGroup>
 
-      <ModalButton label="Save" onClick={handleClose} />
+      {/* <ModalButton label="Save" onClick={handleClose} /> */}
     </BaseModal>
   );
 }
