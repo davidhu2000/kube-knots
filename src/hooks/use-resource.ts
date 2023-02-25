3e1cfe6576a744ac7c440d76dadd7f37c4102cdf
David Hu <davidhu314@gmail.com>
2023-02-25 15:19:03 -0800

    switch-context

--- src/hooks/use-resource.ts
+++ src/hooks/use-resource.ts
@@ -2,15 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api";
 
 import { useCurrentNamespace } from "../namespaces/namespaces";
+import { useCurrentContext } from "../providers/current-context-provider";
 
 type Commands = "get_config";
 
 export function useResource<T>(command: Commands) {
   const { namespace } = useCurrentNamespace();
+  const { currentContext } = useCurrentContext();
   const result = useQuery(
     [command, namespace],
     () => {
-      return invoke<T>(command, { namespace });
+      return invoke<T>(command, { namespace, context: currentContext });
     },
     // TODO: maybe make this configurable?
     { refetchInterval: 2000 }
