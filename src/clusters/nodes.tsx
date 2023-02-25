3569fb6d7f03a313cdb59cfbb56e9aa186334e07
David Hu <davidhu314@gmail.com>
2023-02-24 21:06:17 -0800

    fix

--- src/clusters/nodes.tsx
+++ src/clusters/nodes.tsx
@@ -1,9 +1,18 @@
 import type { NodeMetric, V1Node } from "@kubernetes/client-node";
 import { formatDistance } from "date-fns";
+import { lazy, Suspense } from "react";
 
+import { ActionGroup, ActionButton } from "../components/action-group";
 import { CpuUsage, MemoryUsage } from "../components/resource-usage";
 import { Table, TableHeader, TableBody, TableCell } from "../components/table";
+import { useResourceActions } from "../hooks/use-resource-actions";
 import { useResourceList } from "../hooks/use-resource-list";
+
+const ResourceEditDrawer = lazy(() =>
+  import("../components/resource-edit-drawer").then((module) => ({
+    default: module.ResourceEditDrawer,
+  }))
+);
 
 export function Nodes() {
   const {
@@ -13,6 +22,8 @@ export function Nodes() {
   const {
     data: { items: nodeMetrics },
   } = useResourceList<NodeMetric>("get_node_metrics");
+
+  const { selected, handleOpen, handleClose, action } = useResourceActions<V1Node, "edit">();
 
   return (
     <div>
@@ -44,11 +55,29 @@ export function Nodes() {
                       addSuffix: true,
                     })}
                 </TableCell>
+
+                <TableCell>
+                  <ActionGroup>
+                    <ActionButton
+                      label="edit"
+                      position="single"
+                      onClick={() => handleOpen(item, "edit")}
+                    />
+                  </ActionGroup>
+                </TableCell>
               </tr>
             );
           })}
         </TableBody>
       </Table>
+
+      <Suspense fallback={<div>Loading Form</div>}>
+        <ResourceEditDrawer
+          isOpen={action === "edit"}
+          handleClose={handleClose}
+          selectedResource={selected}
+        />
+      </Suspense>
     </div>
   );
 }
