33c1a699bb6feaceba8d023f66032d22dc8beed9
David Hu <davidhu314@gmail.com>
2023-02-25 14:38:40 -0800

    fix

--- src-tauri/src/core/config.rs
+++ src-tauri/src/core/config.rs
@@ -12,31 +12,3 @@ pub fn get_config() -> Result<Kubeconfig, String> {
         Err(e) => Err(e.to_string()),
     };
 }
-
-#[tauri::command]
-pub async fn set_context(context: String) -> Result<Client, String> {
-    let kubeconfig = Kubeconfig::read().unwrap();
-
-    let possible_context = kubeconfig.contexts.iter().find(|c| c.name == context);
-
-    if possible_context.is_none() {
-        return Err(format!("Context {} not found", context));
-    }
-
-    let options = KubeConfigOptions {
-        context: Some(context.clone()),
-        cluster: None,
-        user: None,
-    };
-
-    let config = Config::from_custom_kubeconfig(kubeconfig, &options)
-        .await
-        .unwrap();
-
-    let a = Client::try_from(config);
-
-    match a {
-        Ok(client) => Ok(client),
-        Err(e) => Err(e.to_string()),
-    }
-}
