09c3ab8b391a2d6f4d750a9df98c23df8dcf224b
David Hu <davidhu314@gmail.com>
2023-02-27 20:24:57 -0800

    test

--- src-tauri/src/core/config.rs
+++ src-tauri/src/core/config.rs
@@ -1,11 +1,30 @@
 use kube::config::Kubeconfig;
 
 #[tauri::command]
-pub fn get_config() -> Result<Kubeconfig, String> {
-    let result = Kubeconfig::read();
+pub fn get_config() {
+    match Command::new("brew").arg("--version").spawn() {
+        Ok(e) => {
+            warn!("get_client_with_context ok 1 brew --version: {:?}", e);
+            let e1 = e.wait_with_output();
 
-    return match result {
-        Ok(kubeconfig) => Ok(kubeconfig),
-        Err(e) => Err(e.to_string()),
+            match e1 {
+                Ok(e1) => {
+                    warn!("get_client_with_context ok 2 brew --version: {:?}", e1);
+                }
+                Err(e1) => {
+                    warn!("get_client_with_context error 2 brew --version: {:?}", e1);
+                }
+            }
+        }
+        Err(e) => {
+            warn!("get_client_with_context error 1 brew --version: {:?}", e);
+        }
     };
+
+    // let result = Kubeconfig::read();
+
+    // return match result {
+    //     Ok(kubeconfig) => Ok(kubeconfig),
+    //     Err(e) => Err(e.to_string()),
+    // };
 }
