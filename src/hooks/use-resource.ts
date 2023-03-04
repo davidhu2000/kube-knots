6514b14ccc8007313d0ee837cbc867d7416eee80
David Hu <davidhu314@gmail.com>
2023-03-04 14:45:13 -0800

    fix

--- src/hooks/use-resource.ts
+++ src/hooks/use-resource.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api";
+import { toast } from "react-toastify";
 
 import { useCurrentContext } from "../providers/current-context-provider";
 import { useNamespace } from "../providers/namespaces-provider";
@@ -9,9 +10,15 @@ type Commands = "get_config";
 export function useResource<T>(command: Commands) {
   const { currentNamespace } = useNamespace();
   const { currentContext } = useCurrentContext();
-  const result = useQuery([command, currentContext, currentNamespace], () => {
-    return invoke<T>(command, { namespace: currentNamespace, context: currentContext });
-  });
+  const result = useQuery(
+    [command, currentContext, currentNamespace],
+    () => {
+      return invoke<T>(command, { namespace: currentNamespace, context: currentContext });
+    },
+    {
+      onError: (error) => toast.error(error as string),
+    }
+  );
 
   return result;
 }
