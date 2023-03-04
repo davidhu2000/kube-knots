fcf4d936c838a1595c4af2bb8b6a336b909996af
David Hu <davidhu314@gmail.com>
2023-03-04 14:49:29 -0800

    fix: remove source column on events-table (#134)

--- src/clusters/events.tsx
+++ src/clusters/events.tsx
@@ -8,13 +8,12 @@ export function Events() {
   return (
     <ResourceTable<CoreV1Event>
       command="get_events"
-      headers={["Reason", "Message", "Source", "Last Seen", "Action"]}
+      headers={["Reason", "Message", "Last Seen", "Action"]}
       actions={["edit"]}
       renderData={(item) => (
         <>
           <TableCell>{item.reason}</TableCell>
           <TableCell>{item.message}</TableCell>
-          <TableCell>{item.source?.component}</TableCell>
           <TableCell>{formatDateString(item.lastTimestamp)}</TableCell>
         </>
       )}
