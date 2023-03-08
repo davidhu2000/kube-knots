9c0631b7f45bb9f11123748b9e8b7f562559e27e
David Hu <davidhu314@gmail.com>
2023-03-08 08:16:04 -0800

    reload-on-error

--- src/components/query-wrapper.tsx
+++ src/components/query-wrapper.tsx
@@ -6,11 +6,18 @@ interface QueryWrapperProps extends Required<PropsWithChildren> {
   query: ReturnType<typeof useResourceList>;
 }
 
-function WrapperContent({ title, subtitle }: { title: string; subtitle: string }) {
+interface WrapperContentProps {
+  title: string;
+  subtitle: string;
+  action?: JSX.Element;
+}
+
+function WrapperContent({ title, subtitle, action }: WrapperContentProps) {
   return (
     <div className="p-8 text-center">
       <p className="text-base font-semibold dark:text-gray-400">{subtitle}</p>
-      <h1 className="mt-4 text-2xl font-bold tracking-tight dark:text-gray-400">{title}</h1>
+      <h1 className="my-4 text-2xl font-bold tracking-tight dark:text-gray-400">{title}</h1>
+      {action}
     </div>
   );
 }
@@ -22,8 +29,28 @@ export function QueryWrapper({ query, children }: QueryWrapperProps) {
   }
 
   if (query.isError) {
-    return <WrapperContent title={JSON.stringify(query.error)} subtitle="Uh Oh" />;
+    return (
+      <WrapperContent
+        title={JSON.stringify(query.error)}
+        subtitle="Uh Oh"
+        action={
+          <button
+            onClick={() => location.reload()}
+            className="rounded-md border bg-blue-200 px-4 py-2 text-gray-900 shadow-md hover:bg-blue-300 dark:border-gray-700 dark:bg-blue-800 dark:text-gray-100 hover:dark:bg-blue-900"
+          >
+            Reload
+          </button>
+        }
+      />
+    );
   }
+
+  <button
+    onClick={() => location.reload()}
+    className="rounded-md border bg-blue-200 px-4 py-2 text-gray-900 shadow-md hover:bg-blue-300 dark:border-gray-700 dark:bg-blue-800 dark:text-gray-100 hover:dark:bg-blue-900"
+  >
+    Reload
+  </button>;
 
   if (query.isSuccess && query.data.items.length === 0) {
     return <WrapperContent title="No resources found" subtitle="404" />;
