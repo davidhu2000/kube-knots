4e41b9ee9a493b296eadf0157e2d30617b183d37
David Hu <davidhu314@gmail.com>
2023-03-13 20:36:31 -0700

    update-doc-meta

--- app/docs/src/pages/index.tsx
+++ app/docs/src/pages/index.tsx
@@ -1,38 +1,14 @@
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Layout from "@theme/Layout";
-import React from "react";
-
-import "./index.css";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className="relative overflow-hidden p-4 text-center bg-red-500">
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className="">
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
+    <Layout title={siteConfig.title} description="Untangling the web of kubernetes">
+      <header>Header</header>
       <main>
-        <HomepageFeatures />
+        <div>content</div>
       </main>
     </Layout>
   );
