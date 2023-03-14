186e021d4d2d58d3b057e2ec642c9e2201f4a944
David Hu <davidhu314@gmail.com>
2023-03-13 20:19:03 -0700

    docs: configure tailwind  (#187)

---
+++ app/docs/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}", "./docs/**/*.mdx", "./blog/**/*.mdx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+  darkMode: ["class", '[data-theme="dark"]'],
+};
