f97cff762436cbdc563224169e30107df0f0d3ef
David Hu <davidhu314@gmail.com>
2023-03-18 13:29:25 -0700

    fix

--- .github/helpers/update-gist.js
+++ .github/helpers/update-gist.js
@@ -1,23 +1,23 @@
-const gistContent = {
+let gistContent = {
   version: "",
   notes: "",
   pub_date: "",
   platforms: {
     "darwin-x86_64": {
       signature: "",
-      url: "https://github.com/davidhu2000/kube-knots/releases/download/v0.0.31/Kube.Knots.app.tar.gz",
+      url: "",
     },
     "darwin-aarch64": {
       signature: "",
-      url: "https://github.com/davidhu2000/kube-knots/releases/download/v0.0.31/Kube.Knots.app.tar.gz",
+      url: "",
     },
     "linux-x86_64": {
       signature: "",
-      url: "https://github.com/davidhu2000/kube-knots/releases/download/v0.0.31/kube-knots_0.0.31_amd64.AppImage.tar.gz",
+      url: "",
     },
     "windows-x86_64": {
       signature: "",
-      url: "https://github.com/davidhu2000/kube-knots/releases/download/v0.0.31/Kube.Knots_0.0.31_x64_en-US.msi.zip",
+      url: "",
     },
   },
 };
@@ -35,9 +35,43 @@ module.exports = async ({ github, context }) => {
   const { data: release } = await github.rest.repos.getRelease(params);
   const { data: assets } = await github.rest.repos.listReleaseAssets(params);
 
+  gistContent.version = release.tag_name;
+  gistContent.notes = release.body;
+  gistContent.pub_date = release.published_at;
+
   console.log(`Release ${release.name} (${release.tag_name}) has ${assets.length} assets:`);
+
   assets.forEach((asset) => {
     console.log(`>>> Processing: ${asset.name}`);
     console.log(asset.browser_download_url);
+
+    // macos x86_64 and aarch64
+    if (asset.name.endsWith(".app.tar.gz")) {
+      gistContent.platforms["darwin-x86_64"].url = asset.browser_download_url;
+      gistContent.platforms["darwin-aarch64"].url = asset.browser_download_url;
+    }
+    if (asset.name.endsWith(".app.tar.gz.sig")) {
+      gistContent.platforms["darwin-x86_64"].signature = "TODO";
+      gistContent.platforms["darwin-aarch64"].signature = "TODO";
+    }
+
+    // linux x86_64
+    if (asset.name.endsWith(".AppImage.tar.gz")) {
+      gistContent.platforms["linux-x86_64"].url = asset.browser_download_url;
+    }
+
+    if (asset.name.endsWith(".AppImage.tar.gz.sig")) {
+      gistContent.platforms["linux-x86_64"].signature = "TODO";
+    }
+
+    // windows x86_64
+    if (asset.name.endsWith(".msi.zip")) {
+      gistContent.platforms["windows-x86_64"].url = asset.browser_download_url;
+    }
+    if (asset.name.endsWith(".msi.zip.sig")) {
+      gistContent.platforms["windows-x86_64"].signature = "TODO";
+    }
   });
+
+  console.log(gistContent);
 };
