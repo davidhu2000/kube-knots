117a68c9a2dec4a9107138225bd50ee6d6c252e0
David Hu <davidhu314@gmail.com>
2023-03-17 19:04:06 -0700

    fix-mac-download

--- app/kube-knots/src/app.tsx
+++ app/kube-knots/src/app.tsx
@@ -1,9 +1,32 @@
 import { RouterProvider } from "@tanstack/react-router";
+import { relaunch } from "@tauri-apps/api/process";
+import { checkUpdate, installUpdate } from "@tauri-apps/api/updater";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 import { AppProviders } from "./app-providers";
 import { router } from "./router";
 
+async function update() {
+  try {
+    const { shouldUpdate } = await checkUpdate();
+    if (shouldUpdate) {
+      // display dialog
+      await installUpdate();
+      // install complete, restart the app
+      await relaunch();
+    }
+  } catch (error) {
+    console.log(error);
+    toast.error(`Failed to update with error: ${error}`);
+  }
+}
+
 export function App() {
+  useEffect(() => {
+    update();
+  }, []);
+
   return (
     <AppProviders>
       <RouterProvider router={router} />
