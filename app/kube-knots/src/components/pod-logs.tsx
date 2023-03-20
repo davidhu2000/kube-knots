bd0cd2df8f2779db0d9836fd5a079ac226a704d3
David Hu <davidhu314@gmail.com>
2023-03-19 22:37:18 -0700

    pog-lot-filter

--- app/kube-knots/src/components/pod-logs.tsx
+++ app/kube-knots/src/components/pod-logs.tsx
@@ -3,7 +3,7 @@ import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { type V1Pod } from "@kubernetes/client-node";
 import { useQuery } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api";
-import { useEffect, useState } from "react";
+import { type ChangeEvent, useCallback, useEffect, useState } from "react";
 
 import { useScrollBottom } from "../hooks/use-scroll-bottom";
 import { useCurrentContext } from "../providers/current-context-provider";
@@ -23,13 +23,18 @@ export function PodLogs({ isOpen, selectedPod, handleClose }: PodLogsProps) {
   const { currentContext } = useCurrentContext();
 
   const [followLogs, setFollowLogs] = useState(true);
-
   const handleFollowLogs = (followLogs: boolean) => {
     setFollowLogs(followLogs);
   };
 
+  const [search, setSearch] = useState("");
+  const handleSearch = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }, []);
+
   useEffect(() => {
     setContainer(selectedPod?.spec?.containers[0].name);
+    setSearch("");
   }, [selectedPod]);
 
   const result = useQuery(
@@ -45,6 +50,12 @@ export function PodLogs({ isOpen, selectedPod, handleClose }: PodLogsProps) {
     { enabled: !!podName, refetchInterval: 1000 }
   );
 
+  const data = result.data?.split("\n") ?? [];
+
+  const filteredData = data.filter((item) => {
+    return item.toLowerCase().includes(search.toLowerCase());
+  });
+
   const logBottomRef = useScrollBottom([result.data, followLogs]);
 
   return (
@@ -53,10 +64,10 @@ export function PodLogs({ isOpen, selectedPod, handleClose }: PodLogsProps) {
       handleClose={handleClose}
       title={selectedPod?.metadata?.name ?? ""}
       description={
-        <div className="flex gap-4">
+        <div className="flex items-center gap-4">
           <Listbox value={container} onChange={(e) => setContainer(e)}>
             <div className="relative z-10 w-60">
-              <Listbox.Button className="relative w-full cursor-default rounded-lg bg-gray-100 py-2 pl-3 pr-10 text-left shadow-md dark:bg-gray-900">
+              <Listbox.Button className="relative w-full cursor-default rounded-lg bg-gray-100 py-2 pl-3 pr-10 text-left shadow-md dark:bg-gray-800">
                 <span className="block truncate">{container}</span>
                 <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                   <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -85,17 +96,25 @@ export function PodLogs({ isOpen, selectedPod, handleClose }: PodLogsProps) {
               </Listbox.Options>
             </div>
           </Listbox>
+
           <ToggleButton
             checked={followLogs}
             onChange={handleFollowLogs}
             checkedLabel="Follow Logs"
             uncheckedLabel="Follow Logs"
           />
+
+          <input
+            value={search}
+            onChange={handleSearch}
+            placeholder="Filter logs..."
+            className="block h-8 w-40 rounded-md p-2 shadow focus:outline-none dark:bg-gray-800 dark:text-gray-300 sm:text-sm"
+          />
         </div>
       }
     >
       <pre className="h-full overflow-y-scroll rounded-md bg-gray-200 p-4 text-sm text-gray-500 dark:bg-gray-800 dark:text-gray-300">
-        {result.data}
+        {filteredData.join("\n")}
         {followLogs && <div ref={logBottomRef} />}
       </pre>
     </Drawer>
