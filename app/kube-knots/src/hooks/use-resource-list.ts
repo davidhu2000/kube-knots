8815d2506750d985d320df3627d05e5e66c006b3
David Hu <davidhu314@gmail.com>
2023-03-23 21:59:34 -0700

    fix

--- app/kube-knots/src/hooks/use-resource-list.ts
+++ app/kube-knots/src/hooks/use-resource-list.ts
@@ -6,7 +6,25 @@ import { useCurrentContext } from "../providers/current-context-provider";
 import { useNamespace } from "../providers/namespaces-provider";
 
 export type ResourceListCommands =
-  "get_config_maps" | "get_cron_jobs" | "get_daemon_sets" | "get_deployments" | "get_events" | "get_horizontal_pod_autoscalers" | "get_ingresses" | "get_jobs" | "get_namespaces" | "get_node_metrics" | "get_nodes" | "get_pod_disruption_budgets" | "get_pod_metrics" | "get_pods" | "get_replica_sets" | "get_replication_controllers" | "get_secrets" | "get_services" | "get_stateful_sets";
+  | "get_config_maps"
+  | "get_cron_jobs"
+  | "get_daemon_sets"
+  | "get_deployments"
+  | "get_events"
+  | "get_horizontal_pod_autoscalers"
+  | "get_ingresses"
+  | "get_jobs"
+  | "get_namespaces"
+  | "get_node_metrics"
+  | "get_nodes"
+  | "get_pod_disruption_budgets"
+  | "get_pod_metrics"
+  | "get_pods"
+  | "get_replica_sets"
+  | "get_replication_controllers"
+  | "get_secrets"
+  | "get_services"
+  | "get_stateful_sets";
 
 export function useResourceList<T>(command: ResourceListCommands, isNamespaced = true) {
   const { currentNamespace } = useNamespace();
