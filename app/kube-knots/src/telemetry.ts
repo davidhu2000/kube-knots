0acff2001e25d2c3cd1b049d2132e598905a2df3
David Hu <davidhu314@gmail.com>
2023-03-24 18:08:39 -0700

    fix: handle condition when umami is undefined on initial launch (#263)

--- app/kube-knots/src/telemetry.ts
+++ app/kube-knots/src/telemetry.ts
@@ -7,8 +7,22 @@ declare global {
   }
 }
 
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function trackAppVersion() {
   const appVersion = await getVersion();
+  let currentWaitTime = 0;
+  while (typeof window.umami !== "function") {
+    currentWaitTime += 1000;
+    console.log("Waiting for window.umami to be defined...");
+    await sleep(1000);
+    if (currentWaitTime > 10000) {
+      return;
+    }
+  }
+
   window.umami(`v${appVersion}`);
 }
 
